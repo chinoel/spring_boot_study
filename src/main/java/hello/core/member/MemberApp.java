04b458a3d92007eeec36d5eba190174a4743a51e
chinoel <tnehd83@naver.com>
2025-03-05 15:18:25 +0900

    study:3-5

--- src/main/java/hello/core/member/MemberApp.java
+++ src/main/java/hello/core/member/MemberApp.java
@@ -1,4 +1,13 @@
 package hello.core.member;
 
 public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member: " + memberA.getName());
+        System.out.println("findMember: " + findMember.getName());
+    }
 }
